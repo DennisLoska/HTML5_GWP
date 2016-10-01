04e5e4555a64898eee5c8c3dc931a78dcd7ced14
DennisTalmid <d.loska@hotmail.de>
2016-10-01 23:38:06 +0200

    Erste JS-Schritte.

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Dennis on 01.10.2016.
+ */
+
+document.getElementById("surveyButton").addEventListener("click", response);
+
+function response() {
+    alert("Vielen Dank für deine Teilnahme!");
+}
+
