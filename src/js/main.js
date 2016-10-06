dc69a2af3388f90c413124343977ac0a9d95a8ee
DennisTalmid <d.loska@hotmail.de>
2016-10-06 22:19:30 +0200

    js remove

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,3 @@
 /**
  * Created by Dennis on 01.10.2016.
  */
-
-document.getElementById("surveyButton").addEventListener("click", response);
-
-function response() {
-    alert("Vielen Dank für deine Teilnahme!");
-
-    
-}
-
