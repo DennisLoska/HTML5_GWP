04a9b733e889f3f1da163bc8e6d399c03a1722e7
DennisTalmid <d.loska@hotmail.de>
2017-01-19 10:09:39 +0100

    form validation

--- src/js/form.js
+++ src/js/form.js
@@ -7,17 +7,32 @@ function validateForm() {
     var grade = document.forms["surveyForm"]["grade"].value;
     var feedBack = document.forms["surveyForm"]["feedBack"].value;
 
+    validateFirstName(firstName);
+    validateLastName(lastName);
     validateName(name);
-
-
-
-
     alert("Vielen dank für deine Teilnahme!");
 }
 
-function validateName(name) {
-
-    alert(name.search(/[a-zA-Z]{1,30}/g));
-
-
+function validateFirstName(firstName) {
+    //checks empty nachname
+    if (firstName == "")
+        alert("Bitte gib deinen Vornamen an!");
+    document.getElementById("firstName").focus();
+    return false;
 }
+
+function validateLastName(lastName) {
+    if (lastName == "")
+        alert("Bitte gib deinen Nachnamen an!");
+    document.getElementById("lastName").focus();
+    return false;
+}
+
+//checks empty vorname
+
+
+function validateName(name) {
+    if (name == "") {
+        alert("Geben Sie Ihren Vor- und Nachnamen an!");
+        return false;
+    }
