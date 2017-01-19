fb3b3e6d1062a4a4863f7774e08d998d75adac79
DennisTalmid <d.loska@hotmail.de>
2017-01-19 10:33:03 +0100

    almost completing formValidation

--- src/js/form.js
+++ src/js/form.js
@@ -9,10 +9,43 @@ function validateForm() {
     var grade = document.forms["surveyForm"]["grade"].value;
     var feedBack = document.forms["surveyForm"]["feedBack"].value;
 
+    validateFeedBack(feedBack);
+    validateGrade(grade);
+    validateGender(gender);
+    validateAge(age);
     validateFirstName(firstName);
     validateLastName(lastName);
     validateName(name);
     alert("Vielen dank für deine Teilnahme!");
+}
+
+function validateFeedBack(feddBack) {
+    if (feddBack == "")
+        alert("Bitte lassen Sie uns mehr über Ihre Meinung erfahren");
+    document.getElementById("feedBack").focus();
+    return false;
+}
+
+function validateGrade(grade) {
+    if (grade == 5)
+        alert("Vielen Dank für Ihre Teilnahme.");
+    else if (grade < 5)
+        alert("Wir hoffen, dass wir unseren Blog so verbessern, dmamit er auch Ihnen gefällt!");
+    else if (grade > 5)
+        alert("Wir frueen uns sehr, dass Ihnen unser Blog gefällt!");
+}
+
+function validateGender(gender) {
+    if (gender == "")
+        alert("Bitte wählen Sie Ihr Geschlecht aus!");
+    return false;
+}
+
+function validateAge(age) {
+    if (age < 5 || age > 130)
+        alert("Bitte geben Sie Ihr richtiges Alter an!");
+    document.getElementById("age").focus();
+    return false;
 }
 
 function validateFirstName(firstName) {
@@ -30,11 +63,8 @@ function validateLastName(lastName) {
     return false;
 }
 
-//checks empty vorname
-
-
 function validateName(name) {
-    if (name == "") {
+    if (name == "")
         alert("Geben Sie Ihren Vor- und Nachnamen an!");
-        return false;
-    }
+    return false;
+}
