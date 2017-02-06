90e11479fbca620611be5d000f439c1d4269e629
DennisTalmid <d.loska@hotmail.de>
2017-02-06 20:19:01 +0100

    basic implementation for improved memory game (beta)

---
+++ js/memory2.js
@@ -0,0 +1,28 @@
+"use strict";
+/*
+ *Shows the game/memory-cards and invokes the function game()
+ */
+function start() {
+
+}
+/*
+ *Shows the solution for the memory game.
+ */
+function solution() {}
+/*
+ *Resets the memory game so you can start over again
+ *A random-function would be nice
+ */
+function reset() {}
+/*
+ *A listener is added to every memory-card. If a card is clicked the listener gets triggered and responses according to its function-body.
+ *
+ */
+function game() {}
+/*
+ *Main-functrion containing the algorithm for the game. It is hard to understand for beginners,
+ *because it is coded by a beginner.
+ */
+function addCardsListener(i) {
+
+}
